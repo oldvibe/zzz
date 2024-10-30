37d735069864c514a228af36d9bf2fa11fc0b601
oldvibe <bobies431@gmail.com>
2024-10-30 18:43:45 +0100

    .h

--- libft.a/libft.h
+++ libft.a/libft.h
@@ -1,24 +1,26 @@
 #include <string.h>
 
-int ft_atoi(const char *str);
+int     ft_atoi(const char *str);
 void	ft_bzero(void *s, int n);
-int ft_isalnum(int i);
-int ft_isalpha(int i);
-int ft_isascii(int i);
-int ft_isdigit(int i);
-int ft_isprint(int i);
-void *ft_memchr(const void *s, int c, size_t n);
-int ft_memcmp(const void *s1, const void *s2, size_t n);
-void *ft_memcpy(void *dest, const void *src, size_t n);
-void *ft_memmove(void *dest, const void *src, size_t n);
-char *ft_memset(void *s, int c, size_t n);
-size_t	ft_strlcpy(char *dest, const char *src, size_t destsize);
-int ft_strncmp(const char *s1, const char *s2, size_t n);
-char *ft_strncpy(char *dest, const char *src, size_t n);
-size_t ft_strlcat(char *dest, const char *src, size_t destsize);
-char *ft_strrchr(const char *s, int c);
-char *ft_strstr(const char *haystack, const char *needle);
-int ft_tolower(int c);
-int ft_toupper(int c);
-int ft_strlen(const char *str);
+int     ft_isalnum(int i);
+int     ft_isalpha(int i);
+int     ft_isascii(int i);
+int     ft_isdigit(int i);
+int     ft_isprint(int i);
+void    *ft_memchr(const void *s, int c, size_t n);
+int     ft_memcmp(const void *s1, const void *s2, size_t n);
+void    *ft_memcpy(void *dest, const void *src, size_t n);
+void    *ft_memmove(void *dest, const void *src, size_t n);
+char    *ft_memset(void *s, int c, size_t n);
+size_t  ft_strlcpy(char *dest, const char *src, size_t destsize);
+int     ft_strncmp(const char *s1, const char *s2, size_t n);
+char    *ft_strncpy(char *dest, const char *src, size_t n);
+size_t  ft_strlcat(char *dest, const char *src, size_t destsize);
+char    *ft_strrchr(const char *s, int c);
+char    *ft_strchr(const char *s, int c);
+char    *ft_strnstr(const char *haystack, const char *needle);
+int     ft_tolower(int c);
+int     ft_toupper(int c);
+int     ft_strlen(const char *str);
+
 
